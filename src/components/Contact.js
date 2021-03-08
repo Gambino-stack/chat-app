95db9fc9f2cdc4d9ddbd11438a3cb44b5669c86b
yohan <yohan.lgd@gmail.com>
2021-03-08 15:38:16 +0100

    quete react 5 - le state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './contact.css';
 
-function Contact(props) {
-    return(
-        <div className="contact">
-            <img className="avatar" src={props.avatar} alt="avatar" />
-            <div>
-            <h4 className="name">{props.name}</h4>
-            <div className="status">
-            <ul className={props.online ? "status-online" : "status-offline"}>
-                 <li>{props.online ? "online" : "offline"}</li> 
-            </ul>  
+class Contact extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            online: this.props.online
+        };
+    }
+    render(){
+        return(
+            <div className="contact">
+                <img className="avatar" src={this.props.avatar} alt="avatar" />
+                <div>
+                <h4 className="name">{this.props.name}</h4>
+                <div className="status" onClick={event => {
+                    const isOnline = !this.state.online;
+                    this.setState({online: isOnline});
+                }}>
+                <ul className={this.state.online ? "status-online" : "status-offline"}>
+                    <li>{this.state.online ? "online" : "offline"}</li> 
+                </ul>  
+                    </div>
                 </div>
+
             </div>
-
-        </div>
-
-    );
+        );
+    }
 }
 
 export default Contact;
