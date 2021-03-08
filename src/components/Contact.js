69609575a573eadb16817be2e330e520928ac66a
yohan <yohan.lgd@gmail.com>
2021-03-08 13:54:08 +0100

    Quete react 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './contact.css';
 
-const isOnline = true;
-function Contact() {
+function Contact(props) {
     return(
         <div className="contact">
-            <img className="avatar" src="https://randomuser.me/api/portraits/women/74.jpg" alt="avatar" />
+            <img className="avatar" src={props.avatar} alt="avatar" />
             <div>
-            <h4 className="name">Louise dupas</h4>
+            <h4 className="name">{props.name}</h4>
             <div className="status">
-            {isOnline ?<ul className="status-online">
-                 <li>En ligne</li> 
-            </ul> : 
-            <ul className="status-offline">
-                <li>Hors-ligne</li>
-                </ul>}
+            <ul className={props.online ? "status-online" : "status-offline"}>
+                 <li>{props.online ? "online" : "offline"}</li> 
+            </ul>  
                 </div>
             </div>
 
